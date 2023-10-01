9c1f23d960b2f40ecdcd07df10e492da4c102174
“philippe <philippe.mail.perso@gmail.com>
2023-10-01 10:02:44 +0200

    more legible code for resources.php

--- locales/en/resources.php
+++ locales/en/resources.php
@@ -9,4 +9,10 @@
  * @copyright 	Olivier Meunier & Association Dotclear
  * @copyright 	GPL-2.0-only
  */
-\Dotclear\App::backend()->resources->set('help', 'resume', __DIR__ . '/help/help.html');
+declare(strict_types=1);
+
+namespace Dotclear\Theme\resume;
+
+use Dotclear\App;
+
+App::backend()->resources->set('help', 'resume', __DIR__ . '/help/help.html');
