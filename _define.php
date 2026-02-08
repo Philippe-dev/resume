f535e13c7a87c51d5f5b1afa28d37a8c8ed20972
“philippe <philippe.mail.perso@gmail.com>
2026-02-08 09:38:00 +0100

    remove non social icons

--- _define.php
+++ _define.php
@@ -12,9 +12,9 @@ $this->registerModule(
     'Resume',
     'A simple Bootstrap 5 blog theme',
     'Philippe aka amalgame and contributors',
-    '5.0',
+    '6.0',
     [
-        'date'        => '2025-12-22T00:00:08+0100',
+        'date'        => '2026-02-08T00:00:08+0100',
         'requires'    => [['core', '2.36']],
         'standalone_config' => true,
         'type'              => 'theme',
